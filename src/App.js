7880f7b21d2cf78349e2a90b479166b734a753a1
git-hafizh <hahafizhmaulana@gmail.com>
2020-09-12 20:47:24 +0700

    adding sass loader

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import "./App.css";
 import WeatherLayout from "./components/WeatherLayout/WeatherLayout";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    }
-  }
-
   render() {
     return(
       <div>
