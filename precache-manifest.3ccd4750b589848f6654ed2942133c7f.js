774122a495a18e2a06bed7d1c05fbdf85a99079a
git-hafizh <hahafizhmaulana@gmail.com>
2020-09-12 20:52:17 +0700

    Updates

---
+++ precache-manifest.3ccd4750b589848f6654ed2942133c7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3c298906dfda9375bafdbb083c75f0",
+    "url": "/weather-app-web-react/index.html"
+  },
+  {
+    "revision": "79921b38df72decb3020",
+    "url": "/weather-app-web-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "af6699a01ab361eafdfa",
+    "url": "/weather-app-web-react/static/css/main.bb19a463.chunk.css"
+  },
+  {
+    "revision": "79921b38df72decb3020",
+    "url": "/weather-app-web-react/static/js/2.fa7dbfbe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/weather-app-web-react/static/js/2.fa7dbfbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6699a01ab361eafdfa",
+    "url": "/weather-app-web-react/static/js/main.f1623905.chunk.js"
+  },
+  {
+    "revision": "7c23046f2a2dfbfa621b",
+    "url": "/weather-app-web-react/static/js/runtime-main.b73ca695.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app-web-react/precache-manifest.090050d0e03b8b9e721f0812b1a7e46c.js"
+  "/weather-app-web-react/precache-manifest.3ccd4750b589848f6654ed2942133c7f.js"
 );
 
 self.addEventListener('message', (event) => {
